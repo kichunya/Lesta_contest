2470f83fc227458d836f74b2afd86d95be2b7aa1
Kichin Egor <kichin.ea@gmail.com>
2022-07-14 10:14:55 +0300

    init

---
+++ Lesta_contest/first_task.py
@@ -0,0 +1,28 @@
+import timeit
+
+
+def isEven_first(value):
+    return value % 2 == 0
+
+
+def isEven_second(value):
+    return bin(value)[-1] == '0'
+
+
+def main():
+    time = 0
+    for i in range(1, 10000, 5):
+        time += timeit.timeit(lambda: isEven_first(i))
+    print(f"first time: {time}")
+
+    time = 0
+    for i in range(1, 10000, 5):
+        time += timeit.timeit(lambda: isEven_first(i))
+    print(f"second time: {time}")
+
+
+if __name__ == '__main__':
+
+    main()
+
+
---
+++ Lesta_contest/second_task.py
@@ -0,0 +1,70 @@
+"""
+#1 List based FIFO
+Inserting and deleting from the beginning of the list takes O(n) time, which is not optimal.
+"""
+
+
+class Fifo1:
+    def __init__(self):
+        self.fifo = []
+
+    def push(self, elem):
+        self.fifo.append(elem)
+
+    def pop(self):
+        return self.fifo.pop(0)
+
+
+"""
+#2 collection.deque
+Inserting and deleting elements from both ends in this case is O(1), 
+since deque is based on a doubly linked list.
+"""
+from collections import deque
+
+
+class Fifo2:
+    def __init__(self):
+        self.fifo = deque()
+
+    def push(self, elem):
+        self.fifo.append(elem)
+
+    def pop(self):
+        return self.fifo.popleft()
+
+
+"""
+#3 multiprocessing.Queue
+This option is used for process-based parallel operation. 
+"""
+from multiprocessing import Queue
+
+
+class Fifo3:
+    def __init__(self):
+        self.fifo = Queue()
+
+    def push(self, elem):
+        self.fifo.put(elem)
+
+    def pop(self):
+        return self.fifo.get()
+
+
+"""
+#4 queue.Queue
+Fifo for multi-threaded use 
+"""
+from queue import Queue
+
+
+class Fifo4:
+    def __init__(self):
+        self.fifo = Queue()
+
+    def push(self, elem):
+        self.fifo.put(elem)
+
+    def pop(self):
+        return self.fifo.get()
